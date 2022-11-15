090ac657ad0e59ea06654a1c9f78e5d43fa24956
Flemmli97 <Flemmli97@users.noreply.github.com>
2022-11-15 16:16:32 +0100

    flicker worn armor also when reviving

--- common/src/main/java/io/github/flemmli97/advancedgolems/ArmorCacheGetter.java
+++ common/src/main/java/io/github/flemmli97/advancedgolems/ArmorCacheGetter.java
@@ -14,9 +14,9 @@ public class ArmorCacheGetter {
     //Reflection. Cause... the accessor mixin prevents hotswapping. idk why
     @SuppressWarnings("unchecked")
     public static ResourceLocation getOrCompute(String s) {
-        if(ARMOR_LOCATION_CACHE == null) {
-            for(Field f : HumanoidArmorLayer.class.getDeclaredFields()) {
-                if(Modifier.isStatic(f.getModifiers()) && Map.class.isAssignableFrom(f.getType())) {
+        if (ARMOR_LOCATION_CACHE == null) {
+            for (Field f : HumanoidArmorLayer.class.getDeclaredFields()) {
+                if (Modifier.isStatic(f.getModifiers()) && Map.class.isAssignableFrom(f.getType())) {
                     try {
                         f.setAccessible(true);
                         ARMOR_LOCATION_CACHE = (Map<String, ResourceLocation>) f.get(null);
